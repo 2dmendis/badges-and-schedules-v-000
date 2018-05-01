655de9133905be2e4588ff5af6496a2e5a2d0bbc
Dmitri Mendis <2dmendis@gmail.com>
2018-05-01 19:28:14 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,22 @@
 def badge_maker(name)
   puts "Hello, my name is #{name}"
 end 
+
+def batch_badge_creator(array)
+  new_array = []
+  array.each do |i|
+    new_array << "Hello my name is #{i}"
+  end
+  new_array
+end 
+
+def assign_rooms(array)
+  room_array = []
+  count = 1 
+  array.each do |name|
+    room_array << "Hello, #{name}! You'll be assigned to room #{count}!"
+    count++
+  end 
+  room_array
+end 
+  
